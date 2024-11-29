8dee999c6aea9e62362ec523d9a1e89bcccf9e38
Subtitletrans <abinvinoy2001@gmail.com>
2024-11-29 18:46:48 +0530

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+
+class cred:
+    BOT_NAME = @Malayalam_Subtitlez
+    BOT_TOKEN = 2042904617:AAGIsBY0LHmdSObXUeGdBLs7D6-x7aFMfKE
+    API_ID = 1087031
+    API_HASH = bb081cad2785a8b8cbc98cdd7be26cca
+    DB_URL = https://trans-ed69c-default-rtdb.firebaseio.com
