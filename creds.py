88d572aae9c4419869841ba59bccb16eb0853c35
Subtitletrans <abinvinoy2001@gmail.com>
2024-11-29 18:49:14 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -6,7 +6,7 @@ load_dotenv(find_dotenv())
 
 class cred:
     BOT_NAME = @Malayalam_Subtitlez
-    BOT_TOKEN = 2042904617:AAGIsBY0LHmdSObXUeGdBLs7D6-x7aFMfKE
+    BOT_TOKEN = 6638369815:AAH42z_xUEGevPxPYDE_Wy6LIJUmblWr028
     API_ID = 1087031
     API_HASH = bb081cad2785a8b8cbc98cdd7be26cca
     DB_URL = https://trans-ed69c-default-rtdb.firebaseio.com
