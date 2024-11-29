b24e669bf1fc0950f0c8bf92726654f6b179fd1c
Subtitletrans <abinvinoy2001@gmail.com>
2024-11-29 19:01:54 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,7 +5,7 @@ load_dotenv(find_dotenv())
 
 
 class cred:
-    BOT_NAME = @Malayalam_Subtitlez
+    BOT_NAME = Malayalam_Subtitlez
     BOT_TOKEN = 6638369815:AAH42z_xUEGevPxPYDE_Wy6LIJUmblWr028
     API_ID = 1087031
     API_HASH = bb081cad2785a8b8cbc98cdd7be26cca
