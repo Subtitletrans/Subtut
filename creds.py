0abf02ef4340e8ce1f8e7f93d1565279dfe16de8
Subtitletrans <abinvinoy2001@gmail.com>
2024-11-29 19:12:59 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,8 +5,12 @@ load_dotenv(find_dotenv())
 
 
 class cred:
-    BOT_NAME = @Malayalam_Subtitlez
-    BOT_TOKEN = 6638369815:AAH42z_xUEGevPxPYDE_Wy6LIJUmblWr028
-    API_ID = 1087031
-    API_HASH = bb081cad2785a8b8cbc98cdd7be26cca
-    DB_URL = https://trans-ed69c-default-rtdb.firebaseio.com
+    BOT_NAME = os.getenv("BOT_NAME")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")  # From botfather
+    API_ID = os.getenv(
+        "API_ID"
+    )  # "Get this value from my.telegram.org! Please do not steal"
+    API_HASH = os.getenv(
+        "API_HASH"
+    )  # "Get this value from my.telegram.org! Please do not steal"
+    DB_URL = os.getenv("DB_URL")  # From Firebase database
